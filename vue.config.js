e1d180f28005bbb97b7e250f96a04afbdf681f40
Pasha <sigmat@protonmail.com>
2021-03-02 09:30:02 -0500

    Updates

--- vue.config.js
+++ vue.config.js
@@ -1,41 +1,41 @@
 const
-	webpack = require('webpack'),
-	fs = require('fs'),
-	packageJson = fs.readFileSync('./package.json'),
-	version = JSON.parse(packageJson).version || 'Unknown';
+webpack = require('webpack'),
+fs = require('fs'),
+packageJson = fs.readFileSync('./package.json'),
+version = JSON.parse(packageJson).version || 'Unknown';
 
 module.exports = {
-	publicPath: '',
-	assetsDir: 'live-atlas',
+  publicPath: '/atlas/',
+  assetsDir: 'live-atlas',
 
-	pluginOptions: {
-        svgSprite: {
-            loaderOptions: {
-                extract: true,
-                spriteFilename: 'live-atlas/img/icons.[hash:8].svg'
-            },
+  pluginOptions: {
+    svgSprite: {
+      loaderOptions: {
+        extract: true,
+        spriteFilename: 'live-atlas/img/icons.[hash:8].svg'
+      },
+    }
+  },
+
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          PACKAGE_VERSION: '"' + version + '"'
         }
-    },
+      })
+    ]
+  },
 
-	configureWebpack: {
-        plugins: [
-            new webpack.DefinePlugin({
-                'process.env': {
-                    PACKAGE_VERSION: '"' + version + '"'
-                }
-            })
-        ]
-    },
-
-	chainWebpack: config => {
-		config.plugin('html')
-			.tap(args => {
-				args[0].minify = false
-				return args
-			})
-		config.module
-			.rule('svg-sprite')
-			.use('svgo-loader')
-			.loader('svgo-loader')
-	}
-}
+  chainWebpack: config => {
+    config.plugin('html')
+      .tap(args => {
+        args[0].minify = false;
+        return args;
+      });
+    config.module
+      .rule('svg-sprite')
+      .use('svgo-loader')
+      .loader('svgo-loader');
+  }
+};
